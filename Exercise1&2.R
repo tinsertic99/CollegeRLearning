9ea79fe8af7d12ff878d89a80c8fdce0ff5c9985
tinsertic99 <62834451+tinsertic99@users.noreply.github.com>
2022-10-10 21:47:27 +0200

    Add files via upload

---
+++ Exercise1&2.R
@@ -0,0 +1,71 @@
+#Vektori
+x1 <- c(1:10)^2
+x1
+x2 <- seq(from = 2,by = 2, length.out = 5)^3
+x2
+x3 <- seq(from = 1, by = 3, length.out = 4)
+x3
+xnew <- c(x1,x2,x3)
+xnew
+xnewy <- append(x3, 0, 0)
+xnewy
+x3 <- c(x3,0)
+x3
+x1[-c(1,2)]
+x1 <- x1[-c(2)]
+x1
+x1 <- x1[-(2:4)]
+x1
+x1 <- c(1:10)^2
+x1
+x1 <- x1[-((length(x1)-1):length(x1))]
+x1
+x1 <- append(x1, 3, 1)
+x1
+x1[x1>=49]
+which(x2 %% 2 == 0)
+
+#Raèunske operacije s vektorima
+x4 <- c(1,5,8)
+sum(x4)
+prod(x4)
+x4^2
+x5 <- sqrt(x4)
+x <- c(1:5)
+x
+pi
+y <- sin(x*pi/2)
+y
+sum(x*y)
+x * 0.1
+#Polako gradimo rjesenje zadatka 10
+seq(from=3, by = 3.5, length.out = 7)
+which(seq(from=3, by = 3.5, length.out = 7)>10)
+prod(which(seq(from=3, by = 3.5, length.out = 7)>10))
+
+#Liste i faktori
+w <- list(x1, voce="ananas", sinusi = y)
+w
+prvi <- w[[1]]
+prvi
+w$voce = "banana"
+w
+w$sinusi=w$sinusi^2
+w
+w[[length(w)+1]] = c("Ivica", "Marica")
+w
+names(w)[length(w)] <- "bajka"
+w
+#Alternativno w$bajka <- c("Ivica", "Marica")
+w$voce = NULL
+w
+#Što se dogodilo?
+sudionici = factor(c("Slovenija", "Austrija", "Austrija", "Slovenija", "Slovenija", "Hrvatska"))
+sudionici
+levels(sudionici) <- c(levels(sudionici), "Maðarska")
+sudionici
+sudionici[length(sudionici)+1] = "Maðarska"
+sudionici
+levels(sudionici)
+levels(sudionici)<-c("Beè", "Zagreb", "Ljubljana", "Budimpešta")
+sudionici
