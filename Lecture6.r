a073879a761b43703f460e4f909fa9d214d1c3c5
tinsertic99 <62834451+tinsertic99@users.noreply.github.com>
2022-11-23 11:29:17 +0100

    Update Lecture6.r

--- Lecture6.r
+++ Lecture6.r
@@ -5,7 +5,7 @@ alpha <-  0.1
 #Nas pokusaj
 L <- qbinom(p=alpha/2, size = 20, prob = 3/4) - 1
 D <- qbinom(p=alpha/2, size = 20, prob = 3/4, lower.tail = FALSE)
-#Lower tail vraæa strogo (strogo veæe od 18), a nama treba veæe ili jednako
+#Lower tail vraÄ‡a strogo (strogo veÄ‡e od 18), a nama treba veÄ‡e ili jednako
 D <- qbinom(p=alpha/2, size = 20, prob = 3/4, lower.tail = FALSE)+1
 #Lijevo kriticno je {0,1,2,3,4,..., 11} U {19,20}
 #Po uputama
@@ -27,9 +27,13 @@ D2 <- qbinom(p=alpha2/2, size = 20, prob = 3/4, lower.tail = FALSE)+1
 L2
 D2
 #Kriticno podrucje je sada {0,1,..,10} U {19,20}
-#Ako smanjimo razinu znaèajnosti, smanjuje se kritièno podruèje.
+#Ako smanjimo razinu znaÄajnosti, smanjuje se kritiÄno podruÄje.
 p1 <- pbinom(q = 12, size = 20, prob = 3/4)
 p2 <- pbinom(q = 11, size = 20, prob = 3/4, lower.tail = FALSE)
 p <- 2 * min(p1,p2)
 #Zato sto je p otprilike 0.2036237, a nase razine znacajnosti su 0.05 i 0.10, sto je manje od naseg p, ne odbacujemo nul hipotezu.
 #Zakljucak da ne odbacujemo nul hipotezu smo mogli dobiti promatrajuci kriticna podrucja.
+
+#2.1 kao sto je Darku rjesio asistent
+#Funkcija jakosti  Pearsonovog testa
+curve(1-pchisq(qchisq(0.95,df = 5), df = 5, ncp=x), from = 0, to = 50)
